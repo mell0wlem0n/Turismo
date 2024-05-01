935a1ab0c87cadad6ba5403bc7d008a7f623decf
stancuvlad-andrei <savandrei.402@gmail.com>
2024-05-01 13:38:06 +0300

    Started working on the UI.

--- app/src/main/java/com/example/turismo/LoadingScreenActivity.java
+++ app/src/main/java/com/example/turismo/LoadingScreenActivity.java
@@ -1,12 +1,17 @@
 package com.example.turismo;
 
 import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.os.Handler;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.bumptech.glide.Glide;
+
 public class LoadingScreenActivity extends AppCompatActivity {
+    private ImageView spinningGlobe;
 
     protected void startApplication() {
         new Handler().postDelayed(new Runnable() {
@@ -16,14 +21,21 @@ public class LoadingScreenActivity extends AppCompatActivity {
                 startActivity(intent);
                 finish();
             }
-        }, 3000);
+        }, 5000);
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loading_screen);
+
+        // Load the GIF into the ImageView
+        ImageView gifProgressBar = findViewById(R.id.spinning_globe);
+        Glide.with(this)
+                .asGif()
+                .load(R.drawable.globe)
+                .into(gifProgressBar);
+
         startApplication();
     }
-
 }
